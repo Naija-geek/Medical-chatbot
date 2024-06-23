3fde7534048dd741ebcdfeaf374dd810eec1bef9
naija-geek <onlineweb.spport@gmail.com>
2024-06-23 23:49:21 +0100

    changed https

--- static/app.js
+++ static/app.js
@@ -31,7 +31,7 @@ class Chatbox {
         let msg1 = { name: "User", message: text1 }
         this.messages.push(msg1);
 
-         fetch('http://0.0.0.0:10000/predict', {
+         fetch('https://0.0.0.0:10000/predict', {
             method: 'POST',
             body: JSON.stringify({ message: text1 }),
             mode: 'cors',
